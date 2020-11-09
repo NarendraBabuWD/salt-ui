2e8cbf87f6d765951f15062987cd2599d8a8ea92
Narendra Babu <narendra.vinodala@gmail.com>
2020-11-09 23:38:54 +0530

    pending components committed

--- src/app/pages/referral/pending-referrals/pending-referrals.component.ts
+++ src/app/pages/referral/pending-referrals/pending-referrals.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { EnumsService } from '../../../@core/data/enums.service';
+import { HttpService } from '../../../@core/services/http.service';
+import { ToastrService } from '../../../@theme/components/toaster/toastr.service';
+import { Router } from '@angular/router';
+// import { LocalDataSource } from 'ng2-smart-table';
+import { NbDialogService } from '@nebular/theme';
+import { ConfirmDeleteComponent } from './../../../@theme/components/modal/confirm-delete/confirm-delete.component';
+
 
 @Component({
   selector: 'ngx-pending-referrals',
@@ -7,7 +15,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PendingReferralsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private toastr: ToastrService,private dialogService: NbDialogService, 
+    private service: HttpService, private enums: EnumsService) { }
 
   pendingReferralsArray = [
     {  patientName: 'Carolina SCHEIN',  alliedHealthName: 'Dr Susan Charlton | Dermatology',
@@ -24,6 +33,64 @@ export class PendingReferralsComponent implements OnInit {
   ];
 
   ngOnInit(): void {
+      this.getPendingReferrals();
   }
+
+  getPendingReferrals() {
+    this.pendingReferralsArray = [];
+    let body = {  id:1, action:"yes"}
+    this.service.post('Refferals/GetReferralByStatus', body, null).subscribe(
+      (response) => {
+        console.log(response);
+        this.pendingReferralsArray = response;
+    },  (error) => {
+      this.toastr.error(error.error);
+    });
+}
+
+sendRef(referral){
+    console.log(referral);
+    // console.log(data);
+  this.dialogService.open(ConfirmDeleteComponent, { hasBackdrop: true,
+  context:{ title:'Send', data:'Are you sure, You want to send this Referral?'} })
+  .onClose.subscribe((res) => {
+    if (res == 'delete') {
+      let deleteById = { id: referral.referral_id};
+      this.service.post('Refferals/SendRefferal', deleteById, null).subscribe(
+        (response) => {
+          this.getPendingReferrals();
+          
+          this.toastr.success('Referrals Sent successfully');
+        },
+        (error) => {
+          this.toastr.error(error.error);
+        });
+    }
+  });
+  }
+
+/*deleteRef(referral){
+    console.log(referral);
+ }*/
+
+ deleteRef(data: any) {
+   console.log(data);
+  this.dialogService.open(ConfirmDeleteComponent, { hasBackdrop: true,
+  context:{ title:'Delete', data:'Are you sure, You want to delete this Referral?'} })
+  .onClose.subscribe((res) => {
+    if (res == 'delete') {
+      let deleteById = { id: data.referral_id};
+      this.service.post('Refferals/DeleteRefferal', deleteById, null).subscribe(
+        (response) => {
+          this.getPendingReferrals();
+          
+          this.toastr.success('Referrals Deleted successfully');
+        },
+        (error) => {
+          this.toastr.error(error.error);
+        });
+    }
+  });
+}
 
 }
