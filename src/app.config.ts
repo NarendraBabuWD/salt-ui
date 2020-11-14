91230ff026ca15ab8defa91233badead494809c1
madhukiranka <narendra.vinodala@gmail.com>
2020-11-14 12:12:36 +0530

    adding owner

--- src/app.config.ts
+++ src/app.config.ts
@@ -8,6 +8,7 @@ if (environment.production) {
 //    serviceApiBaseUrl = 'http://localhost:19323/';
 //    serviceApiBaseUrl = 'http://23.101.222.132/salt/';
      serviceApiBaseUrl = 'http://13.75.138.135/salt/';
+    //  serviceApiBaseUrl = 'https://synap-api.azurewebsites.net/'
      mediaBaseUrl = 'http://media.edunix.in/';
 
 } else {
